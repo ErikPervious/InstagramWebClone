fc3d5e2ad21a3eca1318a4ff6f2ea168f7c29de5
erikpervious <erickmatheus3000@gmail.com>
2022-12-20 15:12:26 -0300

    colors variables added

--- src/styles/colors.ts
+++
@@ -1,9 +0,0 @@
-const colors = {
-  WHITE_PRIMARY: '#FFFFFF',
-  WHITE_SECONDARY: '#FAFAFA',
-  GRAY_PRIMARY: '#DBDBDB',
-  GRAY_SECONDARY: '#7c7c7c',
-  BLACK_PRIMARY: '#262626'
-}
-
-export default colors;
